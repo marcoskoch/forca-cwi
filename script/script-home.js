4c98195452e336831e7cadfd8008b5264e89eb9e
Marcos Koch <marcos_koch@hotmail.com>
2015-10-23 23:26:55 -0200

    feat:(Forca) - iniciar jogo com palavra da dificuldade escolhida

--- script/script-home.js
+++ script/script-home.js
@@ -1,5 +1,5 @@
 function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+	return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
 
 var nome = 'Usuário: '+getURLParameter('nome');
@@ -7,10 +7,38 @@ var dif = 'Dificuldade: ' +getURLParameter('dificuldade');
 $('#nomeLI').text(nome);
 $('#dificuldadeLI').text(dif);
 
+getPalavraPorDificuldade(dif);
+
 function verificarErro(char){
-  var erro = $('#errado').text();
-  erroSom();
-  if(erro.substring(7).indexOf(char) === -1){
-    $('#errado').html(erro + char +' ');
-  }
+	var erro = $('#errado').text();
+	erroSom();
+	if(erro.substring(7).indexOf(char) === -1){
+		$('#errado').html(erro + char +' ');
+	}
 }
+
+function sortearPalavra(){
+	return Math.floor(Math.random() * (5) + 1);
+}
+
+
+function getPalavraPorDificuldade (dificuldade) {
+	var indiceDificuldade;
+	if (dificuldade.toLowerCase() === 'normal') {
+		indiceDificuldade = 0;
+	} else{
+		indiceDificuldade = 1;
+	};
+	var indicePalavra = sortearPalavra();
+	$.get('http://localhost:3000/forca')
+	.done(function(data) {
+		var palavras = data[indiceDificuldade];
+		var palavra = palavras.palavras[0][indicePalavra];
+		for(var i = 0, len = palavra.length; i < len; ++i) {
+			$('#letras').append(
+				$('<li class="inline-block" >').html(palavra[i])
+			);
+		}
+	});
+}
+
